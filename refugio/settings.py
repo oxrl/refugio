c1c2e252adcaa18d0c050c2b7d18cf3825c07e01
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2016-11-30 01:26:51 -0500

    Se agrego la funcionalidad de recuperar clave en el login

--- refugio/settings.py
+++ refugio/settings.py
@@ -132,3 +132,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL=reverse_lazy('adopcion:solicitud_listar')
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'andredelgado1001@gmail.com'
+EMAIL_HOST_PASSWORD = 'cursodjango16'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
