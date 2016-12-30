913dcab8fc8dfa5eab8f8a7912c444bdd267b4df
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2016-12-30 02:22:33 -0500

    Se uso la libreria rest_framework que sirve para crear APIs y creo y se uso al otener la lista de usuarios serializados

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url,include
-from apps.usuario.views import RegistroUsuario
+from apps.usuario.views import RegistroUsuario,UserAPI
 
 urlpatterns = [
     url(r'^registrar', RegistroUsuario.as_view(), name="registrar"),
+    url(r'^api', UserAPI.as_view(), name="api"),
 ]
